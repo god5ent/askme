a8980be712c72143eeeab785ca97a99dfee7f79c
god5ent <policeenemiesfakehomies@gmail.com>
2022-02-08 13:43:49 +0300

    grid fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,14 @@
 import Rails from "@rails/ujs"
 
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
 Rails.start()
-
-ActiveStorage.start()
 
 document.addEventListener('DOMContentLoaded', () => {
     const askButton = document.getElementById('ask-button')
     const askForm = document.getElementById('ask-form')
+
+    window.onresize = () => {
+        askForm.style.display = window.innerWidth < 960 ? 'none' : 'block'
+    }
 
     askButton.addEventListener('click', (event) => {
         event.preventDefault()

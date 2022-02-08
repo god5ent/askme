ac33477b7a82059824af64c0e5efc067bfb861fe
god5ent <policeenemiesfakehomies@gmail.com>
2022-02-08 15:04:10 +0300

    code style update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,17 @@
 import Rails from "@rails/ujs"
 
 Rails.start()
-
 document.addEventListener('DOMContentLoaded', () => {
-    const askButton = document.getElementById('ask-button')
-    const askForm = document.getElementById('ask-form')
-
-    window.onresize = () => {
-        askForm.style.display = window.innerWidth < 960 ? 'none' : 'block'
-    }
-
-    askButton.addEventListener('click', (event) => {
-        event.preventDefault()
-
-        setTimeout(() => {
-            const displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
-            askForm.style.display = displayStyle
-        }, 300)
-    })
+  const askButton = document.getElementById('ask-button')
+  const askForm = document.getElementById('ask-form')
+  window.onresize = () => {
+    askForm.style.display = window.innerWidth < 960 ? 'none' : 'block'
+  }
+  askButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    setTimeout(() => {
+      const displayStyle = askForm.style.display === 'block' ? 'none' : 'block'
+      askForm.style.display = displayStyle
+    }, 300)
+  })
 })

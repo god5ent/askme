0905a53e8c65a96e0bce992eb1da652f08c502e2
god5ent <policeenemiesfakehomies@gmail.com>
2022-02-20 18:34:58 +0300

    add question hashtag relation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
+
   validates :text, presence: true
   validates :text, length: { maximum: 255 }
 end
